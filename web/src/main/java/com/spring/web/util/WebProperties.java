5a98c54bcb42377520a635c9ce43747129570b47
kivensun <kivensun@gmail.com>
2018-02-23 19:59:05 +0800

    First Commit

---
+++ web/src/main/java/com/spring/web/util/WebProperties.java
@@ -0,0 +1,27 @@
+package com.spring.web.util;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class WebProperties {
+    @Value("${com.neo.title}")
+    private String title;
+
+    @Value("${com.neo.description}")
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
